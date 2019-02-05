72817330d575c44bfb5df6a40e71a3b6148938c4
Ivica Kocev <ico.kocev@gmail.com>
2019-02-05 10:51:22 +0100

    Code cleanup

--- src/com/ivica/wordinfile/Main.java
+++ src/com/ivica/wordinfile/Main.java
@@ -1,6 +1,5 @@
 package com.ivica.wordinfile;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
@@ -9,81 +8,45 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.*;
 
 public class Main {
 
     private static final String PATH = "enwik9";
 
-    private static final int NUMBER_OF_THREADS = 16;
+    private static final int NUMBER_OF_THREADS = Runtime.getRuntime().availableProcessors() * 2;
     private static final String WORD = "Euler";
-    private static final int OFFSET = WORD.length() - 1;
-    private static final byte[] W_BYTES = WORD.getBytes();
-    private static final int SIZE = 8192;
+    private static final byte[] WORD_BYTES = WORD.getBytes();
+    private static final int BUFFER_SIZE = 8192;
 
-//    static ExecutorService executor = new ThreadPoolExecutor(NUMBER_OF_THREADS, NUMBER_OF_THREADS,
-//            10_000, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>());
-    static ExecutorService executor = Executors.newFixedThreadPool(16);
+    private static ExecutorService executorService = Executors.newWorkStealingPool(NUMBER_OF_THREADS);
 
-
-    public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
-        findWordInFile();
-        //slowFind();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        splitInChunksAndStartSearch();
     }
 
-    private static void slowFind() throws IOException {
-        FileInputStream inputStream = null;
-        Scanner sc = null;
-        int occurrences = 0;
-        try {
-            inputStream = new FileInputStream(PATH);
-            sc = new Scanner(inputStream, "UTF-8");
-            while (sc.hasNextLine()) {
-                int start = 0;
-                String line = sc.nextLine();
-                while (start < line.length() && line.indexOf(WORD, start) != -1) {
-                    occurrences++;
-                    start = line.indexOf(WORD, start) + WORD.length();
-                }
-            }
-            // note that Scanner suppresses exceptions
-            if (sc.ioException() != null) {
-                throw sc.ioException();
-            }
-        } finally {
-            if (inputStream != null) {
-                inputStream.close();
-            }
-            if (sc != null) {
-                sc.close();
-            }
-        }
-        System.out.println("Total occurrences = " + occurrences);
-    }
-
-
-    private static void findWordInFile() throws InterruptedException, ExecutionException {
+    private static void splitInChunksAndStartSearch() throws InterruptedException, ExecutionException {
         Instant startInterval = Instant.now();
 
         long occurrences = 0;
-        long totalLength = 0;
+        long fileLength = 0;
 
         try(final RandomAccessFile file = new RandomAccessFile(PATH, "r")) {
-            totalLength = file.getChannel().size();
+            fileLength = file.getChannel().size();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        System.out.println("Total file length=" + totalLength);
+        System.out.println("Total file length=" + fileLength);
 
-        long slices = totalLength / NUMBER_OF_THREADS;
+        long slices = fileLength / NUMBER_OF_THREADS;
 
         final List<CompletableFuture<Long>> threads = new ArrayList<>();
         for (int i = 0; i < NUMBER_OF_THREADS; i++) {
-            long startIndex =  i * slices;
+            final long startIndex =  i * slices;
             System.out.println("Spawning thread with start=" + startIndex + " and length=" + slices);
-            threads.add(CompletableFuture.supplyAsync(() -> readFile(startIndex, slices), executor));
+
+            threads.add(CompletableFuture.supplyAsync(() -> readFile(startIndex, slices), executorService));
         }
 
         for (CompletableFuture<Long> thread : threads) {
@@ -91,10 +54,9 @@ public class Main {
         }
 
         Instant finish = Instant.now();
-        System.out.println("Occurrences found=[" + occurrences + "]");
-        System.out.println(Duration.between(startInterval, finish).toMillis() + " ms");
+        System.out.println("Occurrences found=[" + occurrences + "] within " + Duration.between(startInterval, finish).toMillis() + " ms");
 
-        executor.shutdown();
+        executorService.shutdown();
     }
 
 
@@ -104,23 +66,23 @@ public class Main {
         try(final RandomAccessFile file = new RandomAccessFile(PATH, "r")) {
             final MappedByteBuffer mb = file.getChannel().map(FileChannel.MapMode.READ_ONLY, start, length);
 
-            byte[] bufferArray = new byte[SIZE];
-            int nGet;
-            while(mb.hasRemaining()) {
-                nGet = Math.min(mb.remaining(), SIZE);
+            byte[] bufferArray = new byte[BUFFER_SIZE];
+            int bufferSize;
 
-                mb.get(bufferArray, 0, nGet);
+            while(mb.hasRemaining()) {
+                bufferSize = Math.min(mb.remaining(), BUFFER_SIZE);
+
+                mb.get(bufferArray, 0, bufferSize);
 
                 int matchIndex = 0;
-                for (int i = 0; i < nGet; i++) {
-
-                    if (bufferArray[i] == W_BYTES[matchIndex]) {
+                for (int i = 0; i < bufferSize; i++) {
+                    if (bufferArray[i] == WORD_BYTES[matchIndex]) {
                         matchIndex++;
                     } else {
                         matchIndex = 0;
                     }
 
-                    if (matchIndex == W_BYTES.length) {
+                    if (matchIndex == WORD_BYTES.length) {
                         counter++;
                         matchIndex = 0;
                     }
